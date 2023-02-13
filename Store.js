d7a3c747efa7a2714cf1a08608293429b2f2ff3d
HassainAhmed <124057658+HassainAhmed@users.noreply.github.com>
2023-02-13 11:30:09 -0800

    Update Store.js

--- Store.js
+++ Store.js
@@ -1,6 +1,6 @@
 import { createStore,applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
-import rootReducers from './Reducers/CombinedReducers'
+import rootReducers from './CombinedReducers'
 
 const store = createStore( rootReducers,combineReducers,applyMiddleware(thunk));
 
